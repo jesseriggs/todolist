93411b119ec7b74e45865da9b7842e09ef39f054
Jesse Riggs <jesseriggs@u.boisestate.edu>
2020-09-24 07:28:11 -0600

    First file in src directory

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './todolist.css';
+import TodoList from './todolist.js';
+import {Arwes, ThemeProvider, createTheme } from 'arwes';
+
+ReactDOM.render(
+  <React.StrictMode>
+   <ThemeProvider theme={createTheme()}>
+    <Arwes animate show>
+	<TodoList />
+    </Arwes>
+   </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
